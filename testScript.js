9f0258e5f4769a732e9831b8ffd5793d3d1274cf
not-memory <tshureem@gmail.com>
2020-11-05 12:57:44 -0500

    Update testScript.js

--- testScript.js
+++ testScript.js
@@ -4,6 +4,16 @@ function myFunction() {
 
 
 
-function printdate() {
-  document.getElementById("dat").innerHTML = Date();
- }
+// function printdate() {
+//   document.getElementById("dat").innerHTML = Date();
+//  }
+
+ function printdate()
+{
+
+var today = new date;
+
+/*var fullDate = */document.getElementById("dat").innerHTML = "...";
+// fullDate.textContent = today.getTime();
+
+}
