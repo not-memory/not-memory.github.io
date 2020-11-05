864e8794fc3099aa359eaf1b3dba3f6f486e172c
not-memory <tshureem@gmail.com>
2020-11-05 12:16:50 -0500

    Update testScript.js

--- testScript.js
+++ testScript.js
@@ -1,3 +1,15 @@
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
+
+
+
+function printDate()
+{
+
+var today = new date;
+
+var fulldate = document.getElementById("fulldate");
+fulldate.textContent = today.getTime();
+
+}
